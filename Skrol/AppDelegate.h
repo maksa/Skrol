72f60ce4d4bb6dd6df805d6521f3720c1b4b76ab
maksa <milan.maksimovic@gmail.com>
2015-06-24 17:48:29 +0200

    initial commit

---
+++ Skrol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Skrol
+//
+//  Created by Maksa on 6/24/15.
+//  Copyright (c) 2015 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Skrol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Skrol
+//
+//  Created by Maksa on 6/24/15.
+//  Copyright (c) 2015 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
